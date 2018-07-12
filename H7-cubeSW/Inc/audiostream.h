d0a60be166103d0b3c60f7dd0c8f39bf4a69ee20
Matthew Wang <Matthew@nat-oitwireless-inside-vapornet100-d-25987.Princeton.EDU>
2018-07-12 14:14:46 -0400

    lcd working but not midi

---
+++ H7-cubeSW/Inc/audiostream.h
@@ -0,0 +1,168 @@
+/**
+  ******************************************************************************
+  * @file    Audio_playback_and_record/inc/waveplayer.h
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    26-June-2014
+  * @brief   Header for waveplayer.c module.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */   
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __AUDIOSTREAM_H
+#define __AUDIOSTREAM_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+#include "OOPS.h"
+
+#define NUM_OSC 			4
+#define INV_NUM_OSC 		1.0f/NUM_OSC
+
+#define NUM_VOICES 8
+
+#define AUDIO_FRAME_SIZE      512
+#define HALF_BUFFER_SIZE      AUDIO_FRAME_SIZE * 2 //number of samples per half of the "double-buffer" (twice the audio frame size because there are interleaved samples for both left and right channels)
+#define AUDIO_BUFFER_SIZE     AUDIO_FRAME_SIZE * 4 //number of samples in the whole data structure (four times the audio frame size because of stereo and also double-buffering/ping-ponging)
+
+
+#define NUM_BUTTONS 16
+uint8_t buttonValues[NUM_BUTTONS];
+uint8_t buttonValuesPrev[NUM_BUTTONS];
+uint32_t buttonCounters[NUM_BUTTONS];
+uint32_t buttonPressed[NUM_BUTTONS];
+
+extern float testFreq;
+extern uint8_t buttonAPressed;
+extern uint8_t doAudio;
+extern float detuneAmounts[NUM_OSC];
+extern float myVol;
+extern uint16_t buffer_offset;
+extern int16_t audioOutBuffer[AUDIO_BUFFER_SIZE];
+extern float noteperiod;
+tSawtooth* osc[NUM_OSC];
+tPoly* poly;
+tTalkbox* vocoder;
+
+
+/* Exported types ------------------------------------------------------------*/
+typedef enum
+{
+  BUFFER_OFFSET_NONE = 0,  
+  BUFFER_OFFSET_HALF,  
+  BUFFER_OFFSET_FULL,     
+}BUFFER_StateTypeDef;
+
+
+void slideValueChanged(uint16_t value);
+
+void knobValueChanged(uint16_t value);
+
+void buttonOneDown(void);
+
+void buttonOneUp(void);
+
+void buttonTwoDown(void);
+
+void buttonTwoUp(void);
+
+void presetButtonDown(void);
+
+void presetButtonUp(void);
+
+void setFundamental(float fund);
+
+
+/* Exported constants --------------------------------------------------------*/
+
+extern float fundamental_hz;
+extern float fundamental_cm;
+extern float fundamental_m;
+extern float inv_fundamental_m;
+extern float cutoff_offset;
+extern float intPeak;
+extern float floatPeak;
+extern float testDelay;
+extern float slide_tune;
+
+extern float valPerM;
+extern float mPerVal;
+
+#ifdef SAMPLERATE96K
+#define SAMPLE_RATE 96000.f
+#else
+#define SAMPLE_RATE 48000.f
+#endif
+
+#define INV_SAMPLE_RATE 1.f/SAMPLE_RATE 
+#define SAMPLE_RATE_MS (SAMPLE_RATE / 1000.f)
+#define INV_SR_MS 1.f/SAMPLE_RATE_MS
+#define SAMPLE_RATE_DIV_PARAMS SAMPLE_RATE / 3
+#define SAMPLE_RATE_DIV_PARAMS_MS (SAMPLE_RATE_DIV_PARAMS / 1000.f)
+#define INV_SR_DIV_PARAMS_MS 1.f/SAMPLE_RATE_DIV_PARAMS_MS
+
+typedef enum LCDModeType
+{
+	LCDModeDisplayPitchClass = 0,
+	LCDModeDisplayPitchMidi,
+	LCDModeTypeNil,
+	LCDModeCount = LCDModeTypeNil
+} LCDModeType;
+
+typedef enum VocodecButton
+{
+	ButtonA = 0,
+	ButtonB,
+	ButtonUp,
+	ButtonDown,
+	ButtonNil
+} VocodecButton;
+
+typedef enum VocodecMode
+{
+	FormantShiftMode = 0,
+	PitchShiftMode,
+	AutotuneMode,
+	VocoderMode,
+	ModeNil
+} VocodecMode;
+
+extern VocodecMode mode;
+
+void buttonWasPressed(VocodecButton button);
+void buttonWasReleased(VocodecButton button);
+
+extern uint16_t knobValue;
+extern int knobMoved;
+extern int calibrated;
+extern LCDModeType lcdMode;
+
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+void audioInit(I2C_HandleTypeDef* hi2c, SAI_HandleTypeDef* hsaiOut, SAI_HandleTypeDef* hsaiIn, RNG_HandleTypeDef* hrandom, uint16_t* myADCArray);
+
+void audioFrame();
+
+void DMA1_TransferCpltCallback(DMA_HandleTypeDef *hdma);
+void DMA1_HalfTransferCpltCallback(DMA_HandleTypeDef *hdma);
+#endif /* __WAVEPLAYER_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+
---
+++ H7-cubeSW/Src/audiostream.c
@@ -0,0 +1,417 @@
+/* Includes ------------------------------------------------------------------*/
+#include "audiostream.h"
+#include "main.h"
+#include "codec.h"
+
+
+
+// align is to make sure they are lined up with the data boundaries of the cache 
+// at(0x3....) is to put them in the D2 domain of SRAM where the DMA can access them
+// (otherwise the TX times out because the DMA can't see the data location) -JS
+
+
+ALIGN_32BYTES (int16_t audioOutBuffer[AUDIO_BUFFER_SIZE] __ATTR_RAM_D2);
+ALIGN_32BYTES (int16_t audioInBuffer[AUDIO_BUFFER_SIZE] __ATTR_RAM_D2);
+
+float detuneAmounts[NUM_OSC];
+
+#define TOTAL_BUFFERS 4
+
+uint16_t* adcVals;
+
+uint8_t buttonAPressed = 0;
+
+uint8_t doAudio = 0;
+
+uint16_t buffer_offset = 0;
+
+float sample = 0.0f;
+float noteperiod;
+float adcx[8];
+
+float detuneMax = 16.0f;
+uint8_t audioInCV = 0;
+uint8_t audioInCVAlt = 0;
+float myVol = 0.0f;
+
+float audioTickL(float audioIn); 
+float audioTickR(float audioIn);
+void buttonCheck(void);
+
+tSNAC* snac;
+tSOLAD* sola;
+tEnv* env;
+
+tHighpass* hp;
+tFormantShifter* fs;
+tFormantShifter* fs2;
+float inBuffer[4096];
+float outBuffer[4096];
+
+VocodecMode mode = FormantShiftMode;
+
+
+/* PSHIFT vars *************/
+float lastmax;
+int fba = 20;
+int cur_read_block = 2, cur_write_block = 0;
+
+float desPitchRatio = 2.0f;
+
+float notePeriods[128];
+int chordArray[12] = {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0};
+
+int hopSize = 64, windowSize = 64;
+float max, timeConstant = 100, envout, deltamax, radius;
+/**********************************************/
+
+
+HAL_StatusTypeDef transmit_status;
+HAL_StatusTypeDef receive_status;
+
+typedef enum BOOL {
+	FALSE = 0,
+	TRUE
+} BOOL;
+
+
+float nearestPeriod(float period)
+{
+	float leastDifference = fabsf(period - notePeriods[0]);
+	float difference;
+	int index;
+	for(int i = 0; i < 128; i++)
+	{
+		if(chordArray[i%12] == 1)
+		{
+			difference = fabsf(period - notePeriods[i]);
+			if(difference < leastDifference)
+			{
+				leastDifference = difference;
+				index = i;
+			}
+		}
+	}
+	return notePeriods[index];
+}
+
+static void setTimeConstant(float tc)
+{
+    timeConstant = tc;
+    radius = exp(-1000.0f * hopSize * oops.invSampleRate / timeConstant);
+}
+
+static BOOL attackDetect(void)
+{
+    envout = tEnvTick(env);
+
+    if (envout >= 1.0f)
+    {
+        lastmax = max;
+        if (envout > max)
+        {
+            max = envout;
+        }
+        else
+        {
+            deltamax = envout - max;
+            max = max * radius;
+        }
+        deltamax = max - lastmax;
+    }
+
+    fba = fba ? (fba-1) : 0;
+
+    if (fba == 0 && (max > 60 && deltamax > 6))
+    {
+        fba = 5;
+
+        return TRUE;
+    }
+    return FALSE;
+}
+
+
+void audioInit(I2C_HandleTypeDef* hi2c, SAI_HandleTypeDef* hsaiOut, SAI_HandleTypeDef* hsaiIn, RNG_HandleTypeDef* hrand, uint16_t* myADCArray)
+{ 
+	// Initialize the audio library. OOPS.
+	OOPSInit(SAMPLE_RATE, HALF_BUFFER_SIZE, &randomNumber);
+	
+	for(int i = 0; i < 128; i++)
+	{
+		notePeriods[i] = 1.0f / OOPS_midiToFrequency(i);
+	}
+	fs = tFormantShifterInit();
+	fs2 = tFormantShifterInit();
+	//now to send all the necessary messages to the codec
+	AudioCodec_init(hi2c);
+
+	HAL_Delay(100);
+
+	adcVals = myADCArray;
+
+	poly = tPolyInit();
+
+	for (int i = 0; i < NUM_OSC; i++)
+	{
+		osc[i] = tSawtoothInit();
+	}
+
+	vocoder = tTalkboxInit();
+
+	// set up the I2S driver to send audio data to the codec (and retrieve input as well)	
+	transmit_status = HAL_SAI_Transmit_DMA(hsaiOut, (uint8_t *)&audioOutBuffer[0], AUDIO_BUFFER_SIZE);
+	receive_status = HAL_SAI_Receive_DMA(hsaiIn, (uint8_t *)&audioInBuffer[0], AUDIO_BUFFER_SIZE);
+	
+	/* Initialize devices for pitch shifting */
+	snac = tSNAC_init(HALF_BUFFER_SIZE, DEFOVERLAP);
+	sola = tSOLAD_init();
+	env = tEnvInit(windowSize, hopSize);
+
+	hp = tHighpassInit(40.0f);
+
+	tSOLAD_setPitchFactor(sola, 2.0f);
+
+	setTimeConstant(timeConstant);
+
+
+}
+
+
+
+float tempVal = 0.0f;
+uint16_t frameCounter = 0;
+int numSamples = AUDIO_FRAME_SIZE;
+
+void audioFrame()
+{
+	buttonCheck();
+
+	uint16_t i = 0;
+	int16_t current_sample = 0;
+	tSawtoothSetFreq(osc[0], 187.5f * 4.0f);
+
+	for (int cc=0; cc < numSamples; cc++)
+	{
+		inBuffer[(cur_read_block*numSamples)+cc] = (float) (audioInBuffer[buffer_offset+(cc*2)] * INV_TWO_TO_15 * 2);
+		//inBuffer[(cur_read_block*numSamples)+cc] = tSawtoothTick(osc[0]);
+	}
+
+	if (mode == FormantShiftMode)
+	{
+		tFormantShifter_ioSamples(fs, &inBuffer[cur_read_block*numSamples], &outBuffer[cur_write_block*numSamples], numSamples, adcVals[1]*INV_TWO_TO_16*4.0f-2.0f);
+
+		for (int cc=0; cc < numSamples; cc++)
+		{
+			audioOutBuffer[buffer_offset + (cc*2)] = (int16_t) (outBuffer[cur_write_block*numSamples+cc] * TWO_TO_15);
+		}
+	}
+	else if (mode == PitchShiftMode)
+	{
+		float  period, frequency, difference;
+
+		tEnvProcessBlock(env, &inBuffer[cur_read_block*numSamples]);
+		if (attackDetect()==TRUE)
+		{
+			tSOLAD_setReadLag(sola, oops.blockSize);
+		}
+
+		// tSNAC period detection
+		tSNAC_ioSamples(snac, &inBuffer[cur_read_block*numSamples], &outBuffer[cur_write_block*numSamples], numSamples);
+
+		period = tSNAC_getPeriod(snac);
+
+		tSOLAD_setPeriod(sola, period);
+
+		tSOLAD_setPitchFactor(sola, (adcVals[1] * INV_TWO_TO_16) * 3.5f + 0.5f);
+
+		tSOLAD_ioSamples(sola, &inBuffer[cur_read_block*numSamples], &outBuffer[cur_write_block*numSamples], numSamples);
+
+		for (int cc=0; cc < numSamples; cc++)
+		{
+			audioOutBuffer[buffer_offset + (cc*2)] = (int16_t) (tHighpassTick(hp, outBuffer[cur_write_block*numSamples+cc]) * TWO_TO_15);
+		}
+	}
+	else if (mode == AutotuneMode)
+	{
+		float  period, frequency, difference;
+
+		tEnvProcessBlock(env, &inBuffer[cur_read_block*numSamples]);
+		if (attackDetect()==TRUE)
+		{
+			tSOLAD_setReadLag(sola, oops.blockSize);
+		}
+
+		// tSNAC period detection
+		tSNAC_ioSamples(snac, &inBuffer[cur_read_block*numSamples], &outBuffer[cur_write_block*numSamples], numSamples);
+
+		period = tSNAC_getPeriod(snac);
+
+		tSOLAD_setPeriod(sola, period);
+
+		tSOLAD_setPitchFactor(sola, (period*oops.invSampleRate)/nearestPeriod(period*oops.invSampleRate));
+
+		tSOLAD_ioSamples(sola, &inBuffer[cur_read_block*numSamples], &outBuffer[cur_write_block*numSamples], numSamples);
+
+		for (int cc=0; cc < numSamples; cc++)
+		{
+			audioOutBuffer[buffer_offset + (cc*2)] = (int16_t) (tHighpassTick(hp, outBuffer[cur_write_block*numSamples+cc]) * TWO_TO_15);
+		}
+	}
+	else if (mode == VocoderMode)
+	{
+		int16_t current_sample= 0;
+		for (i = 0; i < (HALF_BUFFER_SIZE); i++)
+		{
+			if ((i & 1) == 0) {
+				current_sample = (int16_t)(audioTickL((float) (audioInBuffer[buffer_offset + i] * INV_TWO_TO_15)) * TWO_TO_15);
+			}
+			else
+			{
+				//current_sample = (int16_t)(audioTickR((float) (audioInBuffer[buffer_offset + i] * INV_TWO_TO_15)) * TWO_TO_15);
+			}
+			audioOutBuffer[buffer_offset + i] = current_sample;
+		}
+	}
+
+	cur_read_block++;
+	if (cur_read_block >= TOTAL_BUFFERS)
+		cur_read_block=0;
+
+	cur_write_block++;
+	if (cur_write_block >= TOTAL_BUFFERS)
+		cur_write_block=0;
+
+
+}
+
+float currentFreq = 1.0f;
+
+float rightInput = 0.0f;
+
+float audioTickL(float audioIn) 
+{
+	sample = 0.0f;
+	/*
+	for (int i = 0; i < 4; i++)
+	{
+		tCycleSetFreq(mySine[i], ((((float)adcVals[i]) * INV_TWO_TO_16) * 1000.0f) + 40.0f);
+
+	}
+	*/
+
+	for (int i = 0; i < NUM_OSC; i++)
+	{
+		if (tPolyGetMidiNote(poly, i)->on == OTRUE)
+		{
+			sample += tSawtoothTick(osc[i]);
+		}
+	}
+
+	//sample = tSawtoothTick(osc[0]);
+	//sample = audioIn;
+	sample *= .25f;
+
+	sample = tTalkboxTick(vocoder, sample, audioIn);
+	sample = OOPS_softClip(sample, 0.98f);
+	//sample *= myVol;
+	//sample = audioIn;
+	return sample;
+}
+
+float audioTickR(float audioIn) 
+{
+	rightInput = audioIn;
+	//sample = audioIn;
+	return audioIn;
+}
+
+void buttonWasPressed(VocodecButton button)
+{
+	if (button == ButtonUp)
+	{
+		mode++;
+		if (mode >= ModeNil) mode = 3;
+	}
+	else if (button == ButtonDown)
+	{
+		mode--;
+		if (mode < FormantShiftMode) mode = 0;
+	}
+
+
+}
+
+void buttonWasReleased(VocodecButton button)
+{
+
+}
+
+#define BUTTON_HYSTERESIS 4
+void buttonCheck(void)
+{
+	buttonValues[0] = !HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1);
+	buttonValues[1] = !HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_9);
+	buttonValues[2] = !HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14);
+	buttonValues[3] = !HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12);
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (buttonValues[i] != buttonValuesPrev[i])
+		{
+			if (buttonCounters[i] < BUTTON_HYSTERESIS)
+			{
+				buttonCounters[i]++;
+			}
+			else
+			{
+				if (buttonValues[i] == 1)
+				{
+					buttonPressed[i] = 1;
+					buttonWasPressed(i);
+				}
+				else
+				{
+					buttonPressed[i] = 0;
+					buttonWasReleased(i);
+				}
+				buttonValuesPrev[i] = buttonValues[i];
+				buttonCounters[i] = 0;
+			}
+		}
+	}
+}
+
+
+void HAL_SAI_ErrorCallback(SAI_HandleTypeDef *hsai)
+{
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
+}
+
+void HAL_SAI_TxCpltCallback(SAI_HandleTypeDef *hsai)
+{
+	;
+}
+
+void HAL_SAI_TxHalfCpltCallback(SAI_HandleTypeDef *hsai)
+{
+  ;
+}
+
+
+void HAL_SAI_RxCpltCallback(SAI_HandleTypeDef *hsai)
+{
+	//doAudio = 2;
+	buffer_offset = HALF_BUFFER_SIZE;
+	audioFrame();
+	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);;
+}
+
+void HAL_SAI_RxHalfCpltCallback(SAI_HandleTypeDef *hsai)
+{
+	//doAudio = 1;
+	buffer_offset = 0;
+	audioFrame();
+	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);;
+}
+
